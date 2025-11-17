a3f3c86c45bb1aa45ab202b20f5cab70845a0b56
sachinkumanache <kumanache@gmail.com>
2025-11-17 16:37:29 +0530

    Set up About Us and Contact pages and added RelatedDoctors component

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { assets } from "../assets/assets_frontend/assets";
+import { useNavigate } from "react-router-dom";
 const Footer = () => {
+  const navigate = useNavigate();
   return (
     <div className="md:mx-10 ">
       <div className=" flex flex-col  md:grid md:grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
@@ -18,9 +20,9 @@ const Footer = () => {
         <div>
           <p className="text-xl font-medium mb-5">COMPANY</p>
           <ul className="flex flex-col gap-2 text-gray-600 ">
-            <li className="">Home</li>
-            <li>About us</li>
-            <li>Contact us</li>
+            <li className="cursor-pointer" onClick={() => {navigate("/"); scrollTo(0,0)}}>Home</li>
+            <li className="cursor-pointer" onClick={() =>{navigate("/about"); scrollTo(0,0)}}>About us</li>
+            <li className="cursor-pointer" onClick={() => {navigate("/contact"); scrollTo(0,0)}}>Contact us</li>
             <li>Privacy policy</li>
           </ul>
         </div>
